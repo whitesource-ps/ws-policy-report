2ec3f07e06e524ae3313842be7d02f71260eebef
rammatzkvosky <ram.matzkovsky@whitesourcesoftware.com>
2021-08-16 12:24:28 +0300

    WS policy report - first version

--- policy_report/ws_policy_report.py
+++ policy_report/ws_policy_report.py
@@ -24,10 +24,10 @@ WSPR_PREFIX = 'WSPR_'
 WSPR_ENV_VARS = [WSPR_PREFIX + sub for sub in ('WS_URL', 'USER_KEY', 'ORG_TOKEN', 'PROJECT_PARALLELISM_LEVEL')]
 
 agent_info = "agentInfo"
-AGENT_NAME = "ps-policy-report"
+AGENT_NAME = "policy-report"
 AGENT_VERSION = "0.1.0"
 
-agent_info_details = {"agent": AGENT_NAME, "agentVersion": AGENT_VERSION}
+agent_info_details = {"agent": "ps-"+AGENT_NAME, "agentVersion": AGENT_VERSION}
 
 REQUEST_TYPE = "requestType"
 USER_KEY = "userKey"
